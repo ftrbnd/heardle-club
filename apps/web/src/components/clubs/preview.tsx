0c9fabc40b900ffd58ccbe29e538764fd21230d4
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2025-06-20 17:49:30 -0700

    Add artist images

--- apps/web/src/components/clubs/preview.tsx
+++ apps/web/src/components/clubs/preview.tsx
@@ -1,3 +1,4 @@
+import { getArtist } from '@/actions/spotify';
 import { protocol, rootDomain } from '@/lib/utils';
 import { SelectClub } from '@repo/database/postgres';
 import Image from 'next/image';
@@ -5,6 +6,7 @@ import Link from 'next/link';
 
 export async function ClubPreview({ club }: { club: SelectClub }) {
 	const href = `${protocol}://${club.subdomain}.${rootDomain}`;
+	const artist = await getArtist(club.artistId);
 
 	return (
 		<Link
@@ -14,11 +16,14 @@ export async function ClubPreview({ club }: { club: SelectClub }) {
 			<div className='card bg-base-100 w-96 shadow-sm'>
 				<figure>
 					<Image
-						src='/artist_placeholder.jpg'
+						src={
+							artist.images.find((img) => img)?.url ?? '/artist_placeholder.jpg'
+						}
 						priority
 						alt={club.displayName}
 						width={200}
-						height={100}
+						height={200}
+						className='w-full max-h-48 object-cover'
 					/>
 				</figure>
 				<div className='card-body'>
