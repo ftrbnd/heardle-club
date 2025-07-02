46f7ff2d6474ae79f9d87f50ee1c3817fb08d7c8
ftrbnd <giosalas25@gmail.com>
2025-07-02 15:22:15 -0700

    Create route group and move auth logic into service

--- apps/server/src/modules/auth/index.ts
+++ apps/server/src/modules/auth/index.ts
@@ -1,171 +1,125 @@
 import { AuthModel } from '@/modules/auth/model';
-import {
-	getProviderEndpoint,
-	getTokens,
-} from '@/modules/auth/providers.config';
 import { Auth } from '@/modules/auth/service';
-import {
-	createSession,
-	inactivityTimeoutSeconds,
-	validateSessionToken,
-} from '@/modules/auth/session';
-import { generateSecureRandomString } from '@/utils/random';
-import {
-	addOAuthAccount,
-	getUserByEmail,
-	insertUser,
-} from '@repo/database/api';
-import { OAuth2Tokens } from 'arctic';
+import { validateSessionToken } from '@/modules/auth/session';
 import { Elysia, redirect, status } from 'elysia';
 
-export const auth = new Elysia({ prefix: '/auth' })
-	.macro({
-		currentSession: () => ({
-			async resolve({ cookie }) {
-				const token = cookie.session_token.value;
-				if (!token) return { session: null };
+export const auth = new Elysia({ prefix: '/auth' }).group(
+	'/login/:provider',
+	{
+		params: AuthModel.loginProviderParams,
+		cookie: AuthModel.oauthStateCookies,
+	},
+	(app) =>
+		app
+			.macro({
+				currentSession: () => ({
+					async resolve({ cookie }) {
+						const token = cookie.session_token.value;
+						if (!token) return { session: null };
 
-				const session = await validateSessionToken(token);
-				return { session };
-			},
-		}),
-	})
-	.get(
-		'/login/:provider',
-		async ({ params: { provider }, cookie, set }) => {
-			const { state, url } = await Auth.createAuthorizationURL({ provider });
+						const session = await validateSessionToken(token);
+						return { session };
+					},
+				}),
+			})
+			.get(
+				'/',
+				async ({ params: { provider }, cookie, set }) => {
+					const { state, url } = await Auth.createAuthorizationURL({
+						provider,
+					});
 
-			const providerCookie =
-				provider === 'spotify'
-					? cookie.SPOTIFY_OAUTH_STATE
-					: provider === 'discord'
-						? cookie.DISCORD_OAUTH_STATE
-						: null;
-			if (!providerCookie)
-				return status(
-					400,
-					'Invalid provider cookie' satisfies AuthModel.InvalidProviderCookie
-				);
+					const providerCookie =
+						provider === 'spotify'
+							? cookie.SPOTIFY_OAUTH_STATE
+							: provider === 'discord'
+								? cookie.DISCORD_OAUTH_STATE
+								: null;
+					if (!providerCookie)
+						return status(
+							400,
+							'Invalid provider cookie' satisfies AuthModel.InvalidProviderCookie
+						);
 
-			providerCookie.set({
-				value: state,
-				secure: process.env.NODE_ENV === 'production',
-				maxAge: 1 * 60 * 60, // 1 hour
-				domain:
-					process.env.NODE_ENV === 'production' ? 'heardle.club' : undefined,
-			});
+					providerCookie.set({
+						value: state,
+						secure: process.env.NODE_ENV === 'production',
+						maxAge: 1 * 60 * 60, // 1 hour
+						domain:
+							process.env.NODE_ENV === 'production'
+								? 'heardle.club'
+								: undefined,
+					});
 
-			set.headers = { location: url.toString() };
-			return redirect(url.href, 302);
-		},
-		{
-			params: AuthModel.loginProviderParams,
-			cookie: AuthModel.oauthStateCookies,
-			response: {
-				302: AuthModel.redirectResponse,
-				400: AuthModel.invalidProviderCookie,
-			},
-		}
-	)
-	.get(
-		'/login/:provider/callback',
-		async ({
-			params: { provider },
-			query: { code, state },
-			set,
-			redirect,
-			cookie,
-		}) => {
-			const storedState = (
-				provider === 'spotify'
-					? cookie.SPOTIFY_OAUTH_STATE
-					: provider === 'discord'
-						? cookie.DISCORD_OAUTH_STATE
-						: null
-			)?.value;
-
-			if (!code || !state || !storedState) {
-				return status(401, 'Unauthorized');
-			}
-			if (state !== storedState) {
-				return status(401, 'Unauthorized');
-			}
-
-			let tokens: OAuth2Tokens;
-			try {
-				tokens = await getTokens(provider, code, null);
-			} catch {
-				return status(401, 'Unauthorized');
-			}
-
-			const providerUserResponse = await fetch(getProviderEndpoint(provider), {
-				headers: {
-					Authorization: `Bearer ${tokens.accessToken()}`,
-				},
-			});
-			const providerUserDetails = await providerUserResponse.json();
-
-			const existingUser = await getUserByEmail(providerUserDetails.email);
-			if (existingUser) {
-				const session = await createSession(existingUser.id);
-				await addOAuthAccount({
-					id: generateSecureRandomString(),
-					provider,
-					providerUserId: providerUserDetails.id,
-					userId: existingUser.id,
-				});
-
-				const expiresAt = new Date(
-					session.lastVerifiedAt.getTime() + inactivityTimeoutSeconds * 1000
-				);
-				cookie.session_token.set({
-					httpOnly: true,
-					path: '/',
-					secure: process.env.NODE_ENV === 'production',
-					sameSite: 'lax',
-					expires: expiresAt,
-				});
-
-				set.headers = { location: '/' };
-				return redirect('/', 302);
-			}
-
-			const userId = generateSecureRandomString();
-			const { user } = await insertUser(
-				{
-					id: userId,
-					email: providerUserDetails.email,
+					set.headers = { location: url.toString() };
+					return redirect(url.href, 302);
 				},
 				{
-					id: generateSecureRandomString(),
-					provider,
-					providerUserId: providerUserDetails.id,
-					userId,
+					response: {
+						302: AuthModel.redirectResponse,
+						400: AuthModel.invalidProviderCookie,
+					},
 				}
-			);
+			)
+			.get(
+				'/callback',
+				async ({
+					params: { provider },
+					query: { code, state },
+					set,
+					redirect,
+					cookie,
+				}) => {
+					const storedState = (
+						provider === 'spotify'
+							? cookie.SPOTIFY_OAUTH_STATE
+							: provider === 'discord'
+								? cookie.DISCORD_OAUTH_STATE
+								: null
+					)?.value;
 
-			const session = await createSession(user.id);
-			const expiresAt = new Date(
-				session.lastVerifiedAt.getTime() + inactivityTimeoutSeconds * 1000
-			);
-			cookie.session_token.set({
-				httpOnly: true,
-				path: '/',
-				secure: process.env.NODE_ENV === 'production',
-				sameSite: 'lax',
-				expires: expiresAt,
-			});
+					if (!code || !state || !storedState) {
+						return status(401, 'Unauthorized');
+					}
+					if (state !== storedState) {
+						return status(401, 'Unauthorized');
+					}
 
-			set.headers = { location: '/' };
-			return redirect('/', 302);
-		},
-		{
-			params: AuthModel.loginProviderParams,
-			query: AuthModel.loginCallbackQuery,
-			cookie: AuthModel.oauthStateCookies,
-			response: {
-				401: AuthModel.unauthorized,
-			},
-			currentSession: true,
-		}
-	);
+					const providerUserDetails = await Auth.getProviderUserDetails(
+						provider,
+						code
+					);
+
+					const existingUser = await Auth.getExistingUser(
+						providerUserDetails.email
+					);
+					const { sessionExpiresAt } = await Auth.setUserDetails(
+						provider,
+						existingUser,
+						providerUserDetails
+					);
+
+					cookie.session_token.set({
+						httpOnly: true,
+						path: '/',
+						secure: process.env.NODE_ENV === 'production',
+						sameSite: 'lax',
+						expires: sessionExpiresAt,
+						domain:
+							process.env.NODE_ENV === 'production'
+								? 'heardle.club'
+								: undefined,
+					});
+
+					set.headers = { location: '/' };
+					return redirect('/', 302);
+				},
+				{
+					query: AuthModel.loginCallbackQuery,
+					response: {
+						401: AuthModel.unauthorized,
+					},
+					currentSession: true,
+				}
+			)
+);
--- apps/server/src/modules/auth/service.ts
+++ apps/server/src/modules/auth/service.ts
@@ -1,6 +1,22 @@
 import { generateState } from 'arctic';
 import type { AuthModel } from './model';
-import { getAuthorizationURL } from '@/modules/auth/providers.config';
+import {
+	getAuthorizationURL,
+	getProviderEndpoint,
+	getTokens,
+	OAuthProvider,
+} from '@/modules/auth/providers.config';
+import {
+	addOAuthAccount,
+	getUserByEmail,
+	insertUser,
+} from '@repo/database/api';
+import {
+	createSession,
+	inactivityTimeoutSeconds,
+} from '@/modules/auth/session';
+import { generateSecureRandomString } from '@/utils/random';
+import { SelectUser } from '@repo/database/postgres';
 
 export abstract class Auth {
 	static async createAuthorizationURL({
@@ -10,5 +26,90 @@ export abstract class Auth {
 		const url = getAuthorizationURL(provider, state);
 
 		return { state, url };
+	}
+
+	static async getProviderUserDetails(provider: OAuthProvider, code: string) {
+		const tokens = await getTokens(provider, code, null);
+
+		const providerUserResponse = await fetch(getProviderEndpoint(provider), {
+			headers: {
+				Authorization: `Bearer ${tokens.accessToken()}`,
+			},
+		});
+		const providerUserDetails = await providerUserResponse.json();
+		return providerUserDetails;
+	}
+
+	static async getExistingUser(email: string) {
+		const existingUser = await getUserByEmail(email);
+
+		return existingUser;
+	}
+
+	static async updateExistingUser(
+		existingUser: SelectUser,
+		provider: OAuthProvider,
+		providerUserId: string
+	) {
+		const session = await createSession(existingUser.id);
+		await addOAuthAccount({
+			id: generateSecureRandomString(),
+			provider,
+			providerUserId,
+			userId: existingUser.id,
+		});
+
+		const expiresAt = new Date(
+			session.lastVerifiedAt.getTime() + inactivityTimeoutSeconds * 1000
+		);
+
+		return { sessionExpiresAt: expiresAt };
+	}
+
+	static async registerNewUser(
+		email: string,
+		provider: OAuthProvider,
+		providerUserId: string
+	) {
+		const userId = generateSecureRandomString();
+		const { user } = await insertUser(
+			{
+				id: userId,
+				email,
+			},
+			{
+				id: generateSecureRandomString(),
+				provider,
+				providerUserId,
+				userId,
+			}
+		);
+
+		const session = await createSession(user.id);
+		const expiresAt = new Date(
+			session.lastVerifiedAt.getTime() + inactivityTimeoutSeconds * 1000
+		);
+
+		return { sessionExpiresAt: expiresAt };
+	}
+
+	static async setUserDetails(
+		provider: OAuthProvider,
+		existingUser: SelectUser | null,
+		providerUserDetails: any
+	) {
+		const { sessionExpiresAt } = existingUser
+			? await Auth.updateExistingUser(
+					existingUser,
+					provider,
+					providerUserDetails.id
+				)
+			: await Auth.registerNewUser(
+					providerUserDetails.email,
+					provider,
+					providerUserDetails.id
+				);
+
+		return { sessionExpiresAt };
 	}
 }
