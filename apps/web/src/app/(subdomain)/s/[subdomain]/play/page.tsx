6318eb142727fc537c48c4ea6571da205d0055e1
ftrbnd <giosalas25@gmail.com>
2025-09-23 15:31:50 -0700

    Add Guess component

--- apps/web/src/app/(subdomain)/s/[subdomain]/play/page.tsx
+++ apps/web/src/app/(subdomain)/s/[subdomain]/play/page.tsx
@@ -6,9 +6,17 @@ import { useSubdomain } from '@/client/hooks/use-subdomain';
 
 export default function Page() {
 	const { daily, songs } = useSubdomain();
+	if (!songs || songs.length === 0) return <></>;
 
 	return (
 		<div className='flex-1 flex flex-col'>
+			{/* <Guess
+				guess={{
+					songId: songs[0].id,
+					status: 'album',
+				}}
+				song={songs[0]}
+			/> */}
 			<SongSelect
 				songs={songs}
 				correctSong={daily?.song}
