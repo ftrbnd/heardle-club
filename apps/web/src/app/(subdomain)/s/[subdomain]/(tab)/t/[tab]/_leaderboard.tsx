6bffa487c6c7dfa0009871e78217dbd2f3d893e6
ftrbnd <giosalas25@gmail.com>
2026-02-11 18:25:47 -0800

    Add empty leaderboard alert

--- apps/web/src/app/(subdomain)/s/[subdomain]/(tab)/t/[tab]/_leaderboard.tsx
+++ apps/web/src/app/(subdomain)/s/[subdomain]/(tab)/t/[tab]/_leaderboard.tsx
@@ -17,66 +17,81 @@ export async function Leaderboard({ club }: LeaderboardProps) {
 
 	return (
 		<div className='overflow-x-auto'>
-			<table className='table table-zebra'>
-				{/* head */}
-				<thead>
-					<tr>
-						<th>User</th>
-						<th>Current Streak</th>
-						<th>Max Streak</th>
-						<th>Accuracy</th>
-						<th>Games Played</th>
-						<th>Win Percentage</th>
-					</tr>
-				</thead>
-				<tbody>
-					{statistics.map((stat) => (
-						<tr
-							key={stat.id}
-							className='hover:bg-base-300'>
-							<td>
-								<div className='flex items-center gap-3'>
-									<div className='avatar'>
-										<div className='mask mask-squircle h-12 w-12'>
-											<UserAvatar
-												user={stat.user}
-												imageSize={4}
-											/>
+			{statistics.length === 0 ? (
+				<EmptyLeaderboard />
+			) : (
+				<table className='table table-zebra'>
+					{/* head */}
+					<thead>
+						<tr>
+							<th>User</th>
+							<th>Current Streak</th>
+							<th>Max Streak</th>
+							<th>Accuracy</th>
+							<th>Games Played</th>
+							<th>Win Percentage</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{statistics.map((stat) => (
+							<tr
+								key={stat.id}
+								className='hover:bg-base-300'>
+								<td>
+									<div className='flex items-center gap-3'>
+										<div className='avatar'>
+											<div className='mask mask-squircle h-12 w-12'>
+												<UserAvatar
+													user={stat.user}
+													imageSize={4}
+												/>
+											</div>
+										</div>
+										<div>
+											<div className='font-bold'>{stat.user?.displayName}</div>
 										</div>
 									</div>
-									<div>
-										<div className='font-bold'>{stat.user?.displayName}</div>
-									</div>
-								</div>
-							</td>
-							<td>
-								{stat.currentStreak}
-								<br />
-								<span className='badge badge-ghost badge-sm'>
-									Since {computeStreakStartDate(stat.currentStreak)}
-								</span>
-							</td>
-							<td>{stat.maxStreak}</td>
-							<td>
-								{computeAccuracyPercentage(stat.accuracy, stat.gamesPlayed)}%
-								<br />
-								<span className='badge badge-ghost badge-sm'>
-									{computeMissedGuesses(stat.gamesPlayed, stat.accuracy)}{' '}
-									incorrect guesses
-								</span>
-							</td>
-							<td>{stat.gamesPlayed}</td>
-							<td>
-								{computeWinPercentage(stat.gamesPlayed, stat.gamesWon)}%
-								<br />
-								<span className='badge badge-ghost badge-sm'>
-									{stat.gamesWon} games won
-								</span>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+								</td>
+								<td>
+									{stat.currentStreak}
+									<br />
+									<span className='badge badge-ghost badge-sm'>
+										Since {computeStreakStartDate(stat.currentStreak)}
+									</span>
+								</td>
+								<td>{stat.maxStreak}</td>
+								<td>
+									{computeAccuracyPercentage(stat.accuracy, stat.gamesPlayed)}%
+									<br />
+									<span className='badge badge-ghost badge-sm'>
+										{computeMissedGuesses(stat.gamesPlayed, stat.accuracy)}{' '}
+										incorrect guesses
+									</span>
+								</td>
+								<td>{stat.gamesPlayed}</td>
+								<td>
+									{computeWinPercentage(stat.gamesPlayed, stat.gamesWon)}%
+									<br />
+									<span className='badge badge-ghost badge-sm'>
+										{stat.gamesWon} games won
+									</span>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)}
+		</div>
+	);
+}
+
+function EmptyLeaderboard() {
+	return (
+		<div
+			role='alert'
+			className=' alert alert-info alert-soft m-2 md:m-4'>
+			<span>This club has no statistics yet.</span>
 		</div>
 	);
 }
